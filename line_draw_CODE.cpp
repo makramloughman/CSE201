bf89c245f9cd584ddc6551ee38fc64dcc37958b9
bogdanraonic <56108566+bogdanraonic@users.noreply.github.com>
2019-11-17 19:34:09 +0100

    Update line_draw_CODE.cpp

--- line_draw_CODE.cpp
+++ line_draw_CODE.cpp
@@ -1,21 +1,3 @@
-/*
- * Copyright (c) 2014 University of Michigan, Ann Arbor.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of Michigan, Ann Arbor. The name of the University 
- * may not be used to endorse or promote products derived from this 
- * software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Author: Sugih Jamin
-*/
 #include <stdio.h>
 #include <stdlib.h>
 
