d6a1a23a3e9bac247cdf58956561252a93016d6c
ayahankir <56382928+ayahankir@users.noreply.github.com>
2019-11-21 00:53:27 +0100

    Create segment.hpp

---
+++ segment.hpp
@@ -0,0 +1,48 @@
+#include "point.hpp"
+class Segment{
+
+public:
+    //create endpoints of segment (point1 and point2)
+    Point(double x1, double y1) _point1;
+    Point(double x2, double y2) _point2;
+
+    void createsegment( _point1 , _point2 );
+
+    void display();         //displays segment
+    void displyendpoints(); //displays segment with endpoints 
+
+    void hide();
+    void hidepoints();
+
+    void extandright();     //move right point while staying in same line to extand
+    void extandleft();      //move left point while staying in same line to extand
+
+    void shrinkright();     //move right point while staying in same line to shrink
+    void shrinkleft();      //move right point while staying in same line to shrink
+
+    void rotate(Point(double x, double y), double degree); //rotate around pont(x,y) with some degree(could be negative)
+    
+    void moverightpoint(); //extand or shrink and then rotate (using previous methods)
+    void moveleftpoint();  //same
+
+    void movesegment();
+
+    void point_symmetry(); //symmetry wrt a point
+    void line_symmetry();  //symmetry wrt a line(axis)
+
+    void getlength();
+
+    void pointonsegment(); //creates point on segment
+
+    void copy(); //copying segment
+
+    void parallel(double distance); //creates the parallel at some distance (could be negative)
+    void perpendicular(int distance); //creates the perpendicular at a distance d from _point1 (should be less then length of segment)
+
+    void midpoint(); //give the midpoint of the segment
+
+    void mediator(); //creates the mediator(line)
+
+    
+
+}
