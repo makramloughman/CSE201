0b5611f3c5d4fdff5029df5860aed95299822939
kaylynnn <56113978+kaylynnn@users.noreply.github.com>
2019-11-21 11:44:34 +0100

    Update vector.cpp
    Check with graphics for the arrow

--- vector.cpp
+++ vector.cpp
@@ -3,5 +3,13 @@
 
 Class Vector : public Segment {
   public:
+    void setStart(); //sets the root
+         Point(double x1, double y1) start;
+  
+    void setEnd(); //sets the destination
+          Point(double x2, double y2) end; 
+  
     void setDirection();  //set the pointing arrow to the second point
+    // check with graphic 
+
 };
