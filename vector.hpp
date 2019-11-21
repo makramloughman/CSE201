e07be48aae151988311ecad0201d2895f6d6279d
kaylynnn <56113978+kaylynnn@users.noreply.github.com>
2019-11-21 11:16:35 +0100

    Create vector.hpp
    Class Vector inheriting from class segment

---
+++ vector.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "point.hpp"
+#include "segment.hpp"
+
+// I am here defining a class vector that herites from the class segment where I am adding the attribut direction
+// The arrow is at the second point, and giving the direction 
+
+Class Vector : public Segment {
+  public:
+    void setDirection();  //set the pointing arrow to the second point
+};
+  
