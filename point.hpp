39a4b9cd8bb032364b95b62ef4afc8cc183dbecf
farah.el-euch <farah.el-euch@polytechnique.edu>
2019-11-21 03:50:38 -0500

    Update point.hpp

--- point.hpp
+++ point.hpp
@@ -7,14 +7,13 @@ private:
     double y; 
 
     std::string name; //name of the point (a letter)
-    // shape shape (circle,cross,etc)
+    //color //default color is black rgba(0,0,0)
+    // shape (circle,cross,etc)
 
 public : 
 
     //Creates Point(0,0)
-    Point()//{
-      //  name = "";
-    //}
+    Point();
 
     Point(std::string mystring) //{
       //  name = mystring;
@@ -26,33 +25,30 @@ public :
     void clickpoint(); //creates a point where we click
 
     void display(); //displays the point in the window
-    void displaycoordinates(); //shows the coordinates next to the label in the window yhy
+    void displaycoordinates(); //shows the coordinates next to the label in the window
 
-    void setx(); //changes the x coordinate
-    void sety(); //changes the y coordinate
+    void setx(double a); //changes the x coordinate
+    void sety(double b); //changes the y coordinate
 
     void getx(); //gives the x coordinate
     void gety(); //gives the y coordinate
     
-    // void givename(); //changes the name of the point
+    void givename(std::string n); //changes the name of the point
     // void hidename(); //hides the name but not the point
     // void changeshape(); //changes the shape of the point 
 
-    void changecolour();
-    void hide(); //hides the point
+    void changecolour(R,G,B); 
+    void hide(); //hides the point by setting opacity level to 0
 
     void translate(); //moves the point in the window
     void display_trace(); //display the trace when the point is translate
     
-    void symmetry_point(); //it creates the symmetric point by clicking on the point then the center of symmetry
-    void symmetry_axis(); //creates the symmetric point with respect to the frame axis
+    void symmetry_point(Point A, Point B); //it creates the symmetric point of A wtr to B
+    void symmetry_axisy(); //creates the symmetric point with respect to the y axis
+    void symmetry_axisx(); //creates the symmetric point with respect to the y axis
 
-    void distance(); //gives the distance between two points
-    void angle(); //gives the angle between three points
+    void distance(Point A, Point B); //gives the distance between two points
+    void angle(Point A, Point B, Point C); //gives the angle between three points
 
     ~Point(); //deletes the point
-
-
-    
-
 }
