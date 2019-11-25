512b63960a7fc70d854ae49085311ae57dac3867
kaylynnn <56113978+kaylynnn@users.noreply.github.com>
2019-11-25 14:27:18 +0100

    Update polygone.hpp

--- Polygone/polygone.hpp
+++ Polygone/polygone.hpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <string>
 #include "point.hpp"
+
+list of points 
+
+class Rectangle {
+  class Square {
+  }
+}
+
+class Triangle{
+  class Equilateral Triangle {
+  }
+    void getHeight(); //
+    void getParameter(); //
+    void getCenterofgravity();//
+    void getOrthocenter();//
+    void Circumscribedcercle();//
+    void getInscribedcercle();//
+}
