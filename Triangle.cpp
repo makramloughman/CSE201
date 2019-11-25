fd162c332ff03ecd77dd2129ab878904896fa609
merybeni <56391073+merybeni@users.noreply.github.com>
2019-11-25 14:15:56 +0100

    Create Triangle.cpp

---
+++ Triangle.cpp
@@ -0,0 +1,14 @@
+#include "Triangle.hpp"
+Triangle::Triangle(double point1 ,double point2 ,double point3){
+    this->point1 = point1;
+    this->point2 = point2;
+    this->point3 = point3;
+    
+}
+
+
+
+
+
+
+
