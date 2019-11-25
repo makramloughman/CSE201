7f4dec4bff2e6ee8ad6f49e674f058b91ed152dc
milicabozanic <56113449+milicabozanic@users.noreply.github.com>
2019-11-25 14:14:07 +0100

    Update circle.hpp
    Added some new functions to class Circle

--- circle.hpp
+++ circle.hpp
@@ -1,14 +1,21 @@
+#pragma once
 // include files for point and line
 class Circle{
     private:
         Point center; //center of the circle
         double r; // radius of the circle
     public:
+        Circle(Point p, radius r); //constructor
+        ~Circle(); // destructor
         double getX(); // returns x coordinate of the center
         double getY(); // returns y coordinate of the center
         double getR(); // returns radius
+        double setX(double x); // set x coordinate of the center
+        double setY(double y); // set y coordinate of the center
+        double setR(double r); // set radius of the circle
         double surface(); // returns surface of the circle
         double perimeter(); // returns perimeter of the circle
+        double diameter(); // return diameter of the circle
         Line tangent(Point M); // returns tangents to the circle that contain point M
 };
 
