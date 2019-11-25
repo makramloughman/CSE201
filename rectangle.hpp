ab8f5cbd2c794e888c4913484b6d3134faf457b3
farah.el-euch <farah.el-euch@polytechnique.edu>
2019-11-25 08:44:38 -0500

    Create rectangle.hpp

---
+++ rectangle.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "point.hpp"
+#include "segment.cpp"
+
+class Rectangle {
+public :
+
+Rectangle(); //creates "empty" rectangle
+
+Rectangle(Point(x1,y1),Point(x2,y2),Point(x3,y3),Point(x4,y4)); //creates a rectangle with 4 given points
+
+Area(); //returns the value of the rectangle's area
+
+}
