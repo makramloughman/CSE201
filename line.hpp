47b86ca59ee3f8e2b9fe0b4105d6d1fb6d669b4e
gautieralassaire <57485496+gautieralassaire@users.noreply.github.com>
2019-11-25 13:38:23 +0100

    Update line.hpp

--- line.hpp
+++ line.hpp
@@ -1,1 +1,16 @@
+#pragma once
 
+class Line {
+  
+  private:
+  
+    Point *p1;
+    Point *p2;
+
+  public:
+  
+    Line(Point &p1, Point &p2); //creates a line passing through two points which correspond to the two arguments
+    
+    void clickpoint()
+  
+  
