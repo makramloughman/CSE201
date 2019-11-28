22d7f1b23506f8f5fa3af0bdd22626b14802cc89
milicabozanic <56113449+milicabozanic@users.noreply.github.com>
2019-11-28 10:50:02 +0100

    Update parabola.hpp

--- parabola.hpp
+++ parabola.hpp
@@ -1,10 +1,15 @@
+#pragma once
 //include files for point and line
 class Parabola{
     private:
         Point focus; // the focus of the parablaola
-        Line directix; // the directrix of the parabola
+        Line directrix; // the directrix of the parabola
     public:
+        Parabola(Point f, Line d); // constructor
+        ~Parabola(); // destructor
         Point getF(); // returns the focus of the parabola
+        double getX(); // returns x coordinate of the focus
+        double getY(); // returns y coordinate of the focus
         Line getD(); // returns the directrix of the parabola
         Line symmetric(); // returns the line which separates the parabola into two identical mirror images
         Line tangent(Point M); // returns tangent lines from point M  
