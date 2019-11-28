9103abbdd0e306152e0edb71ac3b052f8a6ca4d6
gautieralassaire <57485496+gautieralassaire@users.noreply.github.com>
2019-11-28 11:10:53 +0100

    Update line.hpp

--- line.hpp
+++ line.hpp
@@ -11,14 +11,14 @@ class Line {
   
     Line(Point &p1, Point &p2); //creates a line passing through two points which correspond to the two arguments
     
-    void semi_line(Point &p1, Point &p2); //creates a semi line from p1 and passing through p2
+    //void semi_line(Point &p1, Point &p2); //creates a semi line from p1 and passing through p2
   
-    void slope(Point &p1,  Point &p2); //computes the slope of the line
+    Line slope(); //computes the slope of the line
     
-    void equation(Point &p1, Point &p2); //computes the equation of the line
+    Line equation(); //computes the equation of the line
   
-    void mediatrice(Point &p1, Point &p2, Point &p3);//draw the medriatrice to the line (p1, p2) passing through p3
+    Line mediatrice(Point &p3);//create the medriatrice to the line passing through p3
   
-    void parllel(Point &p1, Point &p2, Point &p3); //draw the parallel to the line (p1, p2) passing through p3
+    Line parallel(Point &p3); //draw the parallel to the line passing through p3
   
 }
