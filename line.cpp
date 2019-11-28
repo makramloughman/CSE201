c13ad8f0e8c3364daa58590be1295c02d5510e16
gautieralassaire <57485496+gautieralassaire@users.noreply.github.com>
2019-11-28 12:19:22 +0100

    Update line.cpp

--- line.cpp
+++ line.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <cmath>
 
+#include "line.hpp"
+
+Line::Line()
+  
+double Line::slope(){
+  double slope;
+  double x1 = &p1.getx();
+  double x2 = &p2.getx();
+  double y1 = &p1.gety();
+  double y2 = &p2.gety();
+  slope = (y2 - y1) / (x2 - x1);
+  return slope;
+}
+
+double Line::y_intercept(){
+  double y_intercept;
+  double a = Line.slope;
+  double x1 = p1.getx();
+  double y1 = p1.gety();
+  y_intercept = y1 - a*x1;
+  return y_intercept;
+}
+
+Line::mediatrice(Point &p3){
+  double a = Line.slope;
+  double b1 = Line.y_intercept();
+  double x0 = &p3.getx();
+  double y0 = &p3.gety();
+  double b2 = y0 + (1/a)*x0 //the equation of the mediatrice is now y = -(1/a) + b2
+  double xstar;
+  xstar = (b2 - b1)/(a + (1/a);
+  double ystar;
+  ystar = -(1/a)*xstar + b2;
+  Point pstar(xstar, ystar);
+  Line med(*pstar, p3);
+  return med;
+  }
