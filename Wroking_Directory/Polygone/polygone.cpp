5d6c3f7ed458b736f138eb203157f8b709eb6741
farah.el-euch <farah.el-euch@polytechnique.edu>
2019-12-05 03:48:23 -0500

    Update polygone.cpp

--- Wroking_Directory/Polygone/polygone.cpp
+++ Wroking_Directory/Polygone/polygone.cpp
@@ -35,3 +35,36 @@ void Polygone::changecolour(R,G,B)
 {
 	this -> colour = rgba(R,G,B);
 }
+
+ RegularPolygone::RegularPolygone() //empty regular polygone with empty vector of points 
+ {
+  std::vector<point> Pointlist
+ }
+
+ RegularPolygone::RegularPolygone(segment, number){
+	
+ }
+
+ RegularPolygone::centerofcircumscribed(){
+ 	segment s1 = createsegment(Pointlist[0] , Pointlist[1])
+	segement s2 = createsegment(Pointlist[1] , Pointlist[2])
+	 
+	m1 = this -> mediator(s1)
+	m2 = this -> mediator(s2)
+		
+	return intersection(m1,m2) //need this
+ }
+
+ RegularPolygone::circumscribedcircle(){
+	 point c = centerofcircumscribed()
+	 double r = this->distance(c, Pointlist[0])
+	 return Circle(c, r)
+ }
+
+ RegularPolygone::length(){
+	 return this->distance(Pointlist[0],Pointlist[1])
+ }
+
+ RegularPolygone::angle(){
+	 //noidea
+ }
