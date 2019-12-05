e5667993b6b24f77f6ba90b03529e165fa299f93
gautieralassaire <57485496+gautieralassaire@users.noreply.github.com>
2019-12-05 10:48:14 +0100

    Update line.hpp

--- Wroking_Directory/line.hpp
+++ Wroking_Directory/line.hpp
@@ -21,5 +21,5 @@ class Line : public Item
     Line perpendicular(Point &p3);//create the perpendicular to the line passing through p3
   
     Line parallel(Point &p3); //draw the parallel to the line passing through p3
-  
-}
+    
+    Point intersection(Point &p1, Point &p2, Point &p3, Point &p4); //return the point where the two lines intersect
