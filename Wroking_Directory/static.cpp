7aaba2c775b8801b741c9882803a67d8e5389d14
merybeni <56391073+merybeni@users.noreply.github.com>
2019-12-07 21:35:45 +0100

    Update static.cpp

--- Wroking_Directory/static.cpp
+++ Wroking_Directory/static.cpp
@@ -1,20 +1,30 @@
 #include "static.cpp"
 using namespace std;
 
-static Point Inter::interesection(Line line1,Line line2)
+static * Inter::interesection(Line line1,Line line2)
+    Point *array;
+
 {
     double s1=line1.slope();
     double b1=line1.y_intercept();
     double s2=line2.slope();
     double b2=line2.y_intercept();
 
+    if (s1==s2){
+        return NULL
+    }
+    else{
     double x=(b2-b1)/(s2-s1)
     double y=s1*x+b1
-    
-    return Point(x,y)
 
+    array=new Point[1]
+    *array[0]=Point(x,y);
+    return array;
+    }
 }
 static * Inter::interesection(Line line ,Segment segment){
+    Point *array;
+
     Line linebis= Line( segment._point1 , segment._point2);
     double s=linebis.slope();
     double c=linebis.y_intercept();
@@ -22,8 +32,15 @@ static * Inter::interesection(Line line ,Segment segment){
     
     if (0=<inter_point.getx()-segment._point1.getx()/segment._point2.getx()-segment._point1.getx()<=1) and 
     (0=<inter_point.gety()-segment._point1.gety()/segment._point2.gety()-segment._point1.gety()<=1){
+        
+        array= new Point[1];
+        *array[0]=inter_point
 
-
+        return array;
+        
+    }
+    else{
+        return NULL;
     }
 
 }
@@ -45,7 +62,7 @@ static * Inter::intersection(Line line,Circle circle){
     double C=b*b-2c*b+c*c+a*a-r*r;
 
     if (B*B-4*A*C<0){
-        return array;
+        return NULL;   
     }
 
     if (B*B-4*A*C>0){
@@ -58,8 +75,7 @@ static * Inter::intersection(Line line,Circle circle){
         
         *array[0]=Point(x1,y1);
         *array[1]=Point(x2,y2);
-        
-        return array
+        return array;
 
     }
 
@@ -71,8 +87,13 @@ static * Inter::intersection(Line line,Circle circle){
         *array[0]=Point(x1,y1)
 
         return array;
-
     }
+  
+}
+
+static *intersection(Circle circle1,Circle circle2){
+
+    Point *array;
     
 }
 
