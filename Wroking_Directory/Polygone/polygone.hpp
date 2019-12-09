a65b4888f913c1920fde7112710e47837124e997
kaylynnn <56113978+kaylynnn@users.noreply.github.com>
2019-12-09 13:35:19 +0100

    Update polygone.hpp

--- Wroking_Directory/Polygone/polygone.hpp
+++ Wroking_Directory/Polygone/polygone.hpp
@@ -8,7 +8,30 @@ class Polygone:public Item {
   private:
     int color [3];
     std::vector<point> Pointlist; 
-    
+  
+ /* Old version
+  
+  class NodeList {
+    public:
+      int data;
+      NodeList *next;
+
+      // create a single node with data d
+      // and optional next node
+      NodeList(int d,  NodeList* nxt = NULL);
+
+      // delete this node and all successor nodes
+      ~ NodeList();
+  };
+
+  class PointList {
+    private:
+    NodeList *first, *last;
+
+   public:
+    // create an empty list
+    PointList(); */
+
   public : 
     //create empty list
     Polygone();
