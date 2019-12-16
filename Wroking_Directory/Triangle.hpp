0992aa7fac5d501b3c2f67db0e1ad014e1d8ab6a
gautieralassaire <57485496+gautieralassaire@users.noreply.github.com>
2019-12-16 11:53:13 +0100

    Update Triangle.hpp

--- Wroking_Directory/Triangle.hpp
+++ Wroking_Directory/Triangle.hpp
@@ -2,9 +2,9 @@
 class Triangle: public  Item{
     private:
     //creates points for triangle
-    Point point1;
-    Point point2;
-    Point point3;
+    Point  point1 ;
+    Point  point2 ;
+    Point  point3 ;
     
     
     public:
