a390bd7335e163856055dbf6647fef5ceb27c737
kaylynnn <56113978+kaylynnn@users.noreply.github.com>
2019-12-16 14:41:38 +0100

    Update polygone.hpp

--- Wroking_Directory/Polygone/polygone.hpp
+++ Wroking_Directory/Polygone/polygone.hpp
@@ -5,12 +5,15 @@
 #include "circle.hpp"
 
 class Polygone {
-  private:
+  public : 
+  
     std::vector<double> Colour;
+    Colour.pushback(0);
+    Colour.pushback(0);
+    Colour.pushback(0);
     std::vector<point> Pointlist; 
     int size;
 
-  public : 
     //create empty list
     Polygone();
     //create a polygone
@@ -48,11 +51,13 @@ class Polygone {
 };
 
 class RegularPolygone : public Polygone {
-  private :
-  int color[3];
-  std::vector<point> Pointlist;
-  
   public :
+  
+  std::vector<double> Colour;
+  Colour.pushback(0);
+  Colour.pushback(0);
+  Colour.pushback(0);
+  std::vector<point> Pointlist;
   
   RegularPolygone(); //empty regular polygone with empty vector of points 
   RegularPolygone(segment, number); //creates a regular polygone with given number of sides and a segment(two points, length)
