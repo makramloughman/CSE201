6bddf33ad0c636e423c3c7c18df43a39ade31f0a
milicabozanic <56113449+milicabozanic@users.noreply.github.com>
2019-12-19 11:21:29 +0100

    adding focus 1 to hyperbola

--- Wroking_Directory/hyperbola.cpp
+++ Wroking_Directory/hyperbola.cpp
@@ -51,7 +51,30 @@ Line Hyperbola:: tangent(Point M)
 
 Point Hyperbola:: focus1()
 {
-    //TO BE IMPLEMENTED
+	double x1 = getX1();
+	double x2 = getX2();
+	double y1 = getY1();
+	double y2 = getY2();
+	double a = sqrt(pow((x2-x1),2) + pow((y1-y2),2))/2;
+	double c = sqrt(a*a + b*b);
+	double k = (y2-y1)/(x2-x1);
+	double n = y1 - (k * x1);
+	double x01 = (-sqrt(a*a*k*k + a*a -2*a*c*k*k - 2*a*c + c*c*k*k + c*c -k*k*x1*x1 - 2*k*n*x1 + 2*y1*n*k - n*n + 2*n*y1 - y1*y1) - k*n + k*y1 + x1 ) / (1 + k*k);
+	double x02 = (sqrt(a*a*k*k + a*a -2*a*c*k*k - 2*a*c + c*c*k*k + c*c -k*k*x1*x1 - 2*k*n*x1 + 2*y1*n*k - n*n + 2*n*y1 - y1*y1) - k*n + k*y1 + x1 ) / (1 + k*k);
+	double y01 = k * x01 + n;
+	double y02 = k * x02 + n;
+	
+	if (sqrt(pow((x01-x2),2) + pow((y01-y2),2)) > sqrt(pow((x02-x2),2) + pow((y02-y2),2)))
+	{
+		std::cout << "first focus is ( " << x01 << ", " << y01 << " )" << std::endl;
+        return Point(x01, y01);
+	}
+	else
+	{
+		std::cout << "first focus is ( " << x02 << ", " << y02 << " )" << std::endl;
+        return Point(x02, y02);
+	}
+	
 }
 
 Point Hyperbola:: focus2()
