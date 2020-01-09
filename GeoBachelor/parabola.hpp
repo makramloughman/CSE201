f178f58906df4dd5711b1e0c00b4fe3fffc2ce4c
milicabozanic <56113449+milicabozanic@users.noreply.github.com>
2020-01-09 10:34:50 +0100

    adding draw, translate and in_personal_area

--- GeoBachelor/parabola.hpp
+++ GeoBachelor/parabola.hpp
@@ -9,6 +9,11 @@ class Parabola: public MathObject
         Point focus; // the focus of the parablaola
         Line directrix; // the directrix of the parabola
     public:
+    
+        virtual void draw();
+        virtual void translate(double dx, double dy);
+        virtual bool in_personal_area(double x, double y);
+    
         Parabola(Point f, Line d); // constructor
         ~Parabola(); // destructor
         Point getF(); // returns the focus of the parabola
