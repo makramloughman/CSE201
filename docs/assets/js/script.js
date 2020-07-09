f586dfaf08c95b4578c2ee875b5c1c4eb29a6047
makramloughman <54935805+makramloughman@users.noreply.github.com>
2020-07-09 22:18:46 +0300

    web

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,6 +1,5 @@
 var cat = document.getElementsByClassName('octo-arm')[0];
 window.addEventListener("load", showPage);
 function showPage() {
-  cat.classList.remove('js-loading');
   cat.classList.add('js-loaded');
 }
